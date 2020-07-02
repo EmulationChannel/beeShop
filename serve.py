83fb3737ad5addcfd1f77894ff037e9b9aa75e31
Manuel Graña <40329660+manuGMG@users.noreply.github.com>
2020-07-02 19:34:13 -0300

    Add files via upload

---
+++ serve.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8 -*-
+
+import os
+import socket
+import struct
+import sys
+import threading
+import time
+import urllib
+
+try:
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+    from SocketServer import TCPServer
+    from urllib import quote
+    input = raw_input
+except ImportError:
+    from http.server import SimpleHTTPRequestHandler
+    from socketserver import TCPServer
+    from urllib.parse import quote
+
+
+
+accepted_extension = ('.cia', '.tik', '.cetk', '.3dsx')
+hostPort = 8080 # Default value
+
+with open('ip.txt', 'r') as f:
+    target_ip = f.read()
+target_path = sys.argv[1]
+hostIp = [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+hostPort = 8080 # Default
+
+target_path = target_path.strip()
+if not os.path.exists(target_path):
+    print(target_path + ': No such file or directory.')
+    sys.exit(1)
+
+
+print('Preparing data...')
+baseUrl = hostIp + ':' + str(hostPort) + '/'
+
+if os.path.isfile(target_path):
+    if target_path.endswith(accepted_extension):
+        file_list_payload = baseUrl + quote(os.path.basename(target_path))
+        directory = os.path.dirname(target_path)  # get file directory
+    else:
+        print('Unsupported file extension. Supported extensions are: ' + accepted_extension)
+        sys.exit(1)
+
+else:
+    directory = target_path  # it's a directory
+    file_list_payload = ''  # init the payload before adding lines
+    for file in [file for file in next(os.walk(target_path))[2] if file.endswith(accepted_extension)]:
+        file_list_payload += baseUrl + quote(file) + '\n'
+
+if len(file_list_payload) == 0:
+    print('No files to serve.')
+    sys.exit(1)
+
+file_list_payloadBytes = file_list_payload.encode('ascii')
+
+if directory and directory != '.':  # doesn't need to move if it's already the current working directory
+    os.chdir(directory)  # set working directory to the right folder to be able to serve files
+
+print('\nURLs:')
+print(file_list_payload + '\n')
+
+class MyServer(TCPServer):
+    def server_bind(self):
+        import socket
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind(self.server_address)
+
+print('Opening HTTP server on port ' + str(hostPort))
+server = MyServer(('', hostPort), SimpleHTTPRequestHandler)
+thread = threading.Thread(target=server.serve_forever)
+thread.start()
+
+try:
+    print('Sending URL(s) to ' + target_ip + ' on port 5000...')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((target_ip, 5000))
+    sock.sendall(struct.pack('!L', len(file_list_payloadBytes)) + file_list_payloadBytes)
+    while len(sock.recv(1)) < 1:
+        time.sleep(0.05)
+    sock.close()
+except Exception as e:
+    print('An error occurred: ' + str(e))
+    server.shutdown()
+    sys.exit(1)
+
+print('Shutting down HTTP server...')
+server.shutdown()
